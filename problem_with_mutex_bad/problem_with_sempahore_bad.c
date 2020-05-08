7411a96e1e948c00d561b97d3d4eb1759feda88d
griloHBG <henriqueborgesgarcia@gmail.com>
2020-05-08 18:41:01 -0300

    correcting CMakeLists

--- problem_with_mutex_bad/problem_with_sempahore_bad.c
+++ problem_with_mutex_bad/problem_with_sempahore_bad.c
@@ -1,4 +1,4 @@
-// from https://www.geeksforgeeks.org/dining-philosopher-problem-using-semaphores/
+//from https://www.geeksforgeeks.org/dining-philosopher-problem-using-semaphores/
 
 #include <pthread.h>
 #include <semaphore.h>
@@ -26,14 +26,14 @@ void test(int phnum)
         && state[RIGHT] != EATING) {
         // state that eating 
         state[phnum] = EATING;
-
+        
         sleep(2);
-
+        
         printf("Philosopher %d takes fork %d and %d\n",
                phnum + 1, LEFT + 1, phnum + 1);
-
+        
         printf("Philosopher %d is Eating\n", phnum + 1);
-
+        
         // sem_post(&S[phnum]) has no effect 
         // during takefork 
         // used to wake up hungry philosophers 
@@ -45,84 +45,87 @@ void test(int phnum)
 // take up chopsticks 
 void take_fork(int phnum)
 {
-
+    
     sem_wait(&mutex);
-
+    
     // state that hungry 
     state[phnum] = HUNGRY;
-
+    
     printf("Philosopher %d is Hungry\n", phnum + 1);
-
+    
     // eat if neighbours are not eating 
     test(phnum);
-
+    
     sem_post(&mutex);
-
+    
     // if unable to eat wait to be signalled 
     sem_wait(&S[phnum]);
-
+    
     sleep(1);
 }
 
 // put down chopsticks 
 void put_fork(int phnum)
 {
-
+    
     sem_wait(&mutex);
-
+    
     // state that thinking 
     state[phnum] = THINKING;
-
+    
     printf("Philosopher %d putting fork %d and %d down\n",
            phnum + 1, LEFT + 1, phnum + 1);
     printf("Philosopher %d is thinking\n", phnum + 1);
-
+    
     test(LEFT);
     test(RIGHT);
-
+    
     sem_post(&mutex);
 }
 
-noreturn void* philospher(void* num)
+noreturn
+void* philospher(void* num)
 {
-
+    
     while (1) {
-
+        
         int* i = num;
-
+        
         sleep(1);
-
+        
         take_fork(*i);
-
+        
         sleep(0);
-
+        
         put_fork(*i);
     }
 }
 
 int main()
 {
-
+    
+    printf("Extracted from https://www.geeksforgeeks.org/dining-philosopher-problem-using-semaphores/\n");
+    
     int i;
     pthread_t thread_id[N];
-
+    
     // initialize the semaphores 
     sem_init(&mutex, 0, 1);
-
+    
     for (i = 0; i < N; i++)
-
+        
         sem_init(&S[i], 0, 0);
-
+    
     for (i = 0; i < N; i++) {
-
+        
         // create philosopher processes 
         pthread_create(&thread_id[i], NULL,
                        philospher, &phil[i]);
-
+        
         printf("Philosopher %d is thinking\n", i + 1);
     }
-
+    
     for (i = 0; i < N; i++)
-
+        
         pthread_join(thread_id[i], NULL);
 } 
